ebf3da9e37885eb0d500fe35ba99fecf3c5ce193
Ajonastech <Agujonas13@gmail.com>
2025-06-27 12:02:10 +0100

    chore: updated hero

--- components/home/hero/Hero.js
+++ components/home/hero/Hero.js
@@ -13,13 +13,17 @@ export const Hero = () => {
           </h1>
         </Reveal>
         <Reveal>
-        <h2 className={styles.subTitle}>
-            I&apos;m a <span>FrontEnd Developer</span>
+          <h2 className={styles.subTitle}>
+            I&apos;m a <span>Javascript Engineer</span>
           </h2>
         </Reveal>
         <Reveal>
-        <p className={styles.aboutCopy}>
-           With two years of experience under my belt, I love crafting amazing things on the web. From pixel-perfect designs to seamless interactions, I bring websites to life with a sprinkle of magic. Join me on this exciting journey of turning ideas into eye-catching digital experiences. Let&apos;s create something awesome together!
+          <p className={styles.aboutCopy}>
+            With four years of experience building for the web, I’m passionate
+            about creating intuitive, responsive, and visually polished user
+            interfaces. From clean, accessible design to smooth, interactive
+            experiences, I enjoy turning ideas into real-world digital products.
+            Let’s build something great together.
           </p>
         </Reveal>
         <Reveal>
