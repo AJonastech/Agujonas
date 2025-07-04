eb1ccff282675cf2e02144b7c9bac63a3fca90c7
Ajonastech <Agujonas13@gmail.com>
2025-07-04 13:36:41 +0100

    feat: let ship 🚀🚀🚀

--- components/nav/SideBar.js
+++ components/nav/SideBar.js
@@ -61,6 +61,16 @@ export const SideBar = () => {
         initial={{ x: -70 }}
         animate={{ x: 0 }}
         transition={{ duration: 0.5, delay: 0.3 }}
+        href="#youtube"
+        onClick={() => setSelected("youtube")}
+        className={selected === "youtube" ? styles.selected : ""}
+      >
+        Videos
+      </motion.a>
+      <motion.a
+        initial={{ x: -70 }}
+        animate={{ x: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
         href="#experience"
         onClick={() => setSelected("experience")}
         className={selected === "experience" ? styles.selected : ""}
@@ -70,7 +80,7 @@ export const SideBar = () => {
       <motion.a
         initial={{ x: -70 }}
         animate={{ x: 0 }}
-        transition={{ duration: 0.5, delay: 0.4 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
         href="#contact"
         onClick={() => setSelected("contact")}
         className={selected === "contact" ? styles.selected : ""}
