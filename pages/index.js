86b5f864c054aef10e6b84f10b5f3f945818b632
Ajonastech <Agujonas13@gmail.com>
2025-07-10 20:49:36 +0100

    chore: updated favicon

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function home() {
         <title>Jonas Agu | Software Engineer</title>
         <meta name="description" content="Portfolio of Jonas Agu, a professional software engineer specializing in web engineering, using JavaScript, typescript, python and modern frameworks." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/profile.PNG" />
       </Head>
       <Home />
     </>
