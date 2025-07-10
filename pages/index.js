c98d7c3060ef7038d2946e91b94758098fe0411e
Ajonastech <Agujonas13@gmail.com>
2025-07-10 20:03:44 +0100

    feat: updated metadata

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,8 @@ export default function home() {
   return (
     <>
       <Head>
-        <title>Jonas Agu | Web Developer</title>
-        <meta name="description" content="Bleep boop" />
+        <title>Jonas Agu | Software Engineer</title>
+        <meta name="description" content="Portfolio of Jonas Agu, a professional software engineer specializing in web engineering, using JavaScript, typescript, python and modern frameworks." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
